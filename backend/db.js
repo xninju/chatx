bcabb441b72018e101ee45cdb014cb8bce204093
xninju <xninju1@gmail.com>
2025-07-18 12:35:47 +0530

    Create db.js

---
+++ backend/db.js
@@ -0,0 +1,11 @@
+import pkg from 'pg';
+const { Pool } = pkg;
+import dotenv from 'dotenv';
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+export default pool;
